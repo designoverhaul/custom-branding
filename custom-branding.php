dd2c630aab06a43bbf307abdd7fc6ba6a13e5899
designoverhaul <aaronheine@gmail.com>
2017-04-03 09:31:02 -0500

    Update custom-branding.php

--- custom-branding.php
+++ custom-branding.php
@@ -5,7 +5,7 @@ Description: Plugin for white labelling WordPress branding
 Plugin URI: https://github.com/designoverhaul/custom-branding
 Author: Jihan Ahmed
 Author URI: http://www.wpunmarked.com
-Version: 1.0.7
+Version: 1.0.8
 */
 
 if( ! class_exists( 'Smashing_Updater' ) ){
